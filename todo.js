24b00b3c6eea2e73ce9f93db7281d55101121110
myeongjookim <godlob73@me.com>
2021-02-05 21:24:18 +0900

    210205

--- todo.js
+++ todo.js
@@ -2,17 +2,32 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 toDoInput = toDoForm.querySelector("input"),
 toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = 'toDos' ;
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, toDos);
+}
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerHTML = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerHTML = "❌";
     span.innerText = text
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+    )
 
 }
 
@@ -24,8 +39,8 @@ function hadleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem (TODOS_LS);
-    if(toDos !== null){
+    const loadToDos = localStorage.getItem (TODOS_LS);
+    if(loadToDos !== null){
     }
 }
 
